43da417396428662692430bc2dd430814e537c0d
unknown <tcnalumno@TECNOAPRENDO.LOCAL>
2022-01-14 19:31:42 +0100

    dash no funciona
    aun hay errores

--- Assets/Scripts/State Machine/States/DashState.cs
+++ Assets/Scripts/State Machine/States/DashState.cs
@@ -7,6 +7,7 @@ public class DashState : State
     protected D_DashState dashData;
     protected bool animationFinished;
 
+
     public DashState(Entity entity, FiniteStateMachine stateMachine, string animName, D_DashState dashData) : base(entity, stateMachine, animName)
     {
         this.dashData = dashData;
@@ -21,11 +22,13 @@ public class DashState : State
     {
         base.Enter();
         Debug.Log("wow dash");
+        entity.rb.AddForce(entity.movement * 8, ForceMode2D.Impulse);
     }
 
     public override void Exit()
     {
         base.Exit();
+        entity.rb.RemoveForce(entity.movement * 8, ForceMode2D.Impulse);
     }
 
     public override void LogicUpdate()
