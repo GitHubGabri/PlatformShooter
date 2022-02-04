bdf394888a6fc53b78d9783fa9d33438fdf70916
DaikiNeverGonna <daiki.soriano@outlook.com>
2022-02-04 15:54:25 +0100

    Perro

---
+++ Assets/Scripts/ScenesLoader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesLoader : MonoBehaviour
+{
+    public Animator Transition;
+    public float transitionTime;
+    public void OpenMenu()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    public void OpenGame()
+    {
+        StartCoroutine(LoadGame(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        Transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene("MainMenu");
+    }
+    IEnumerator LoadGame(int levelIndex)
+    {
+        Transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene("EscenaPrueba");
+    }
+}
